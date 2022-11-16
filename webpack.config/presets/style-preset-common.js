7af6b6c0121b2e732c7077453fa76b900b3951e4
darthmorgot <dartmorgot@yandex.ru>
2022-11-17 00:57:06 +1000

    Добавил настройки для postcss-loader

--- webpack.config/presets/style-preset-common.js
+++ webpack.config/presets/style-preset-common.js
@@ -1,10 +1,12 @@
+const path = require('../paths.js');
+
 const stylePreset = () => {
   return {
     test: /\.(s[ac]ss|css)$/i,
     use: [
       {loader: 'style-loader'},
       {loader: 'css-loader', options: {sourceMap: true}},
-      {loader: 'postcss-loader', options: {sourceMap: true}},
+      {loader: 'postcss-loader', options: {sourceMap: true, postcssOptions: {config: path.src + '/postcss/postcss.config.js'}}},
       {loader: 'sass-loader', options: {sourceMap: true}}
     ]
   };
