53b1d31002d5d92abf64f5b3850b0c26566f5aa0
darthmorgot <dartmorgot@yandex.ru>
2023-03-02 21:25:19 +1000

    Пытаюсь решить проблему отображения изменений в коде во время запуска сайта на локальном сервере

--- webpack.config/presets/html-preset-common.js
+++ webpack.config/presets/html-preset-common.js
@@ -1,7 +1,8 @@
 const htmlPreset = () => {
   return {
     test: /\.pug$/,
-    loader: '@webdiscus/pug-loader',
+    // loader: '@webdiscus/pug-loader',
+    loader: 'pug-loader',
   };
 };
 
--- webpack.config/webpack.dev.js
+++ webpack.config/webpack.dev.js
@@ -1,3 +1,5 @@
+const pathapp = require('path');
+
 const path = require('./paths.js');
 const webpack = require('webpack');
 
@@ -5,10 +7,14 @@ module.exports = {
   mode: 'development',
   devtool: 'eval-cheap-module-source-map',
   devServer: {
+    host: 'localhost',
     port: 8080,
     open: 'chrome',
     historyApiFallback: true,
-    contentBase: path.build,
+    static: {
+      directory: pathapp.resolve(__dirname, '../dist'),
+    },
+    // contentBase: path.build,
     compress: true,
     hot: true
   },
