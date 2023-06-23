6f8d75b707c1878e4feb9d3b7e42382f8ca6f1ba
darthmorgot <dartmorgot@yandex.ru>
2023-06-23 21:11:51 +1000

    Удалил ненужный код

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import myTestFunc from './scripts/test.js';
-
-console.log('Test index page');
-console.log(`2 * 4 = ${2 * 4}`);
-// console.log(myTestFunc());
-myTestFunc();
+console.log('Index page test');
